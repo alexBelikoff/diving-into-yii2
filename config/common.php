868d79e051be23cc699e4f26e2e28321abc1657c
Alexey <alexey@beluha.pro>
2015-07-19 23:02:00 +0400

    add ProfileController

--- config/common.php
+++ config/common.php
@@ -16,34 +16,34 @@ return [
         'admin' => [
             'class' => 'app\modules\admin\Module',
         ],
-		'main' => [
-			'class' => 'app\modules\main\Module',
-		],
-		'user' => [
-			'class' => 'app\modules\user\Module',
-		],
+            'main' => [
+                    'class' => 'app\modules\main\Module',
+            ],
+            'user' => [
+                    'class' => 'app\modules\user\Module',
+            ],
 	],
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
             'charset' => 'utf8',
         ],
-		'urlManager' => [
-			'class' => 'yii\web\UrlManager',
-			'enablePrettyUrl' => true,
-			'showScriptName' => false,
-			'rules' => [
-				'' => 'main/default/index',
-				'contact' => 'main/contact/index',
-				'<_a:error>' => 'main/default/<_a>',
-				'<_a:(login|logout|signup|confirm-email|request-password-reset|reset-password)>' => 'user/default/<_a>',
+        'urlManager' => [
+                'class' => 'yii\web\UrlManager',
+                'enablePrettyUrl' => true,
+                'showScriptName' => false,
+                'rules' => [
+                        '' => 'main/default/index',
+                        'contact' => 'main/contact/index',
+                        '<_a:error>' => 'main/default/<_a>',
+                        '<_a:(login|logout|signup|confirm-email|request-password-reset|reset-password)>' => 'user/default/<_a>',
 
-                '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
-                '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_m>/<_c>/<_a>',
-                '<_m:[\w\-]+>' => '<_m>/default/index',
-                '<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
-			],
-		],
+        '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
+        '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_m>/<_c>/<_a>',
+        '<_m:[\w\-]+>' => '<_m>/default/index',
+        '<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
+                ],
+        ],
         'authManager' => [
             'class' => 'yii\rbac\PhpManager',
             'defaultRoles' => ['user', 'admin'],
--- modules/user/Module.php
+++ modules/user/Module.php
@@ -7,6 +7,5 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
-        // custom initialization code goes here
     }
 }
